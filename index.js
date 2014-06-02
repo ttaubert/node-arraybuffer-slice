356cadd76687f9613d8ed6a896d72605a4fa1d8c
Tim Taubert <tim@timtaubert.de>
2014-06-02 14:54:48 +0200

    wrap main code in a function expression to not force strict mode for everyone else

--- index.js
+++ index.js
@@ -2,26 +2,28 @@
 // (c) 2014 Tim Taubert <tim@timtaubert.de>
 // arraybuffer-slice may be freely distributed under the MIT license.
 
-"use strict";
+(function (undefined) {
+  "use strict";
 
-if (!ArrayBuffer.prototype.slice) {
-  ArrayBuffer.prototype.slice = function (begin, end) {
-    begin = (begin|0) || 0;
-    var num = this.byteLength;
-    end = end === (void 0) ? num : (end|0);
+  if (!ArrayBuffer.prototype.slice) {
+    ArrayBuffer.prototype.slice = function (begin, end) {
+      begin = (begin|0) || 0;
+      var num = this.byteLength;
+      end = end === undefined ? num : (end|0);
 
-    // Handle negative values.
-    if (begin < 0) begin += num;
-    if (end < 0) end += num;
+      // Handle negative values.
+      if (begin < 0) begin += num;
+      if (end < 0) end += num;
 
-    if (num === 0 || begin >= num || begin >= end) {
-      return new ArrayBuffer(0);
-    }
+      if (num === 0 || begin >= num || begin >= end) {
+        return new ArrayBuffer(0);
+      }
 
-    var length = Math.min(num - begin, end - begin);
-    var target = new ArrayBuffer(length);
-    var targetArray = new Uint8Array(target);
-    targetArray.set(new Uint8Array(this, begin, length));
-    return target;
-  };
-}
+      var length = Math.min(num - begin, end - begin);
+      var target = new ArrayBuffer(length);
+      var targetArray = new Uint8Array(target);
+      targetArray.set(new Uint8Array(this, begin, length));
+      return target;
+    };
+  }
+})();
